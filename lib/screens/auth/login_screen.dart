d479b0c0c91ecfa85d1b1a78d83d4a87ad5e1984
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-06-04 16:43:22 +0000

    flutter_web_auth for auth

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,66 +1,40 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:nuance/utils/constants.dart';
 
-import 'dart:async';
-
-import 'package:uni_links2/uni_links.dart';
-
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
   String _status = '';
-  StreamSubscription? _sub;
-
-  @override
-  void initState() {
-    super.initState();
-    _initDeepLinkListener();
-  }
-
-  @override
-  void dispose() {
-    _sub?.cancel();
-    super.dispose();
-  }
-
-  void _initDeepLinkListener() {
-    _sub = linkStream.listen((String? link) {
-      if (link != null) {
-        Uri uri = Uri.parse(link);
-        String? token = uri.queryParameters['token'];
-        if (token != null) {
-          setState(() {
-            _status = 'Logged in with token: $token';
-          });
-          Navigator.of(context).pushNamed("/");
-          // Navigate to another screen or perform other actions
-        }
-      }
-    }, onError: (err) {
-      setState(() {
-        _status = 'Failed to get deep link: $err';
-      });
-    });
-  }
 
   Future<void> _authenticate() async {
     const authUrl = '$baseURL/auth/login';
     const callbackUrlScheme = "nuance";
 
     try {
-      await FlutterWebAuth.authenticate(
+      final result = await FlutterWebAuth.authenticate(
         url: authUrl,
         callbackUrlScheme: callbackUrlScheme,
       );
+      print("USER RESULT!!!!: $result");
+      log("USER RESULT!!!!: $result");
+
+      // Handle the redirect URI and extract user data
+      // final userAccessToken =
+      //     Uri.parse(result).fragment; // Use fragment instead of queryParameters
+      final userAccessToken = Uri.parse(result).queryParameters['code'];
+      log("USER ACCESS TOKEN: $userAccessToken");
     } on PlatformException catch (e) {
       setState(() {
+        log("ERROR MESSAGE: ${e.message}");
         _status = 'Error: ${e.message}';
       });
     }
