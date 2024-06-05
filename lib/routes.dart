af7621a08993dd548d1a2daa385c920b25fd188f
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-06-05 11:43:04 +0000

    consistent routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,7 +3,7 @@ import 'package:nuance/screens/home_screen.dart';
 import 'package:nuance/screens/auth/login_screen.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
-  '/': (context) => const LoginScreen(),
-  '/home': (context) => const HomeScreen(),
+  LoginScreen.routeName: (context) => const LoginScreen(),
+  HomeScreen.routeName: (context) => const HomeScreen(),
   // Define other routes here
 };
