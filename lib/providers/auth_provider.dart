3caab03fab4c35abc9947465faa1ed2efc77ca1b
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-06-07 10:28:05 +0000

    providers

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,4 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nuance/providers/secure_storage_provider.dart';
 import 'package:nuance/services/auth_service.dart';
 
-final authServiceProvider = Provider<AuthService>((ref) => AuthService());
+final authServiceProvider = Provider<AuthService>((ref) {
+  final secureStorage = ref.read(secureStorageProvider);
+  return AuthService(secureStorage);
+});
