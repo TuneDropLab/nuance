ea8c6905d8d53a5a39935e17c003cc551ae17de2
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-06-07 10:29:51 +0000

    recommendation provider calls the service

--- lib/providers/recommendation_provider.dart
+++ lib/providers/recommendation_provider.dart
@@ -1,18 +1,19 @@
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:nuance/models/recommendation_model.dart';
-// import 'package:nuance/providers/auth_provider.dart';
-// import 'package:nuance/services/spotify_api_services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nuance/providers/auth_provider.dart';
+import 'package:nuance/services/recomedation_service.dart';
 
-// final apiServiceProvider = Provider<ApiService>((ref) {
-//   return ApiService();
-// });
+import 'package:nuance/models/recommendation_model.dart';
 
-// final recommendationProvider =
-//     FutureProvider<List<RecommendationModel>>((ref) async {
-//   final authModel = ref.watch(authStateProvider);
-//   if (authModel == null) {
-//     throw Exception('Not authenticated');
-//   }
-//   final apiService = ref.read(apiServiceProvider);
-//   return apiService.fetchRecommendations(authModel.accessToken);
-// });
+final recommendationsProvider =
+    FutureProvider<List<RecommendationModel>>((ref) async {
+  final authService = ref.read(authServiceProvider);
+  
+  final sessionData = await authService.getSessionData();
+
+  if (sessionData == null) {
+    throw Exception('User not authenticated');
+  }
+
+  return await RecommendationsService()
+      .getRecommendations(sessionData['access_token']);
+});
