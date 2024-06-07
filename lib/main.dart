fdc37c5ee6eee6a5b9d0157bb0fc4050a87bbb28
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-06-07 10:25:45 +0000

    initial route based on sessiondata

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,43 @@
-// src/main.dart
+// main.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
+import 'package:nuance/providers/auth_provider.dart';
 import 'package:nuance/routes.dart';
+import 'package:nuance/screens/auth/login_screen.dart';
+import 'package:nuance/screens/home_screen.dart';
+import 'package:nuance/theme.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MyApp());
+  final container = ProviderContainer();
+  final authService = container.read(authServiceProvider);
+  final sessionData = await authService.getSessionData();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        authServiceProvider.overrideWithValue(authService),
+      ],
+      child: MyApp(sessionData: sessionData),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Map<String, dynamic>? sessionData;
+
+  const MyApp({super.key, this.sessionData});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Nuance',
-      initialRoute: '/',
+      initialRoute:
+          sessionData == null ? LoginScreen.routeName : HomeScreen.routeName,
+      theme: AppTheme.lightTheme,
       routes: routes,
     );
   }
