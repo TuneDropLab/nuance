3b661f3b1721784493145c045698e57cff56f64a
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-06-07 10:26:21 +0000

    home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nuance/models/session_data_model.dart';
+import 'package:nuance/providers/session_notifier.dart';
 
-class HomeScreen extends StatefulWidget {
-  final String? accessToken;
-  static var routeName = '/home';
+class HomeScreen extends ConsumerWidget {
+  static const routeName = '/home';
 
-  const HomeScreen({super.key, this.accessToken});
+  const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sessionData = ref.watch(sessionProvider);
 
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("accessToken: ${widget.accessToken ?? ""}"),
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFF6A88E5),
+                Color(0xFF00B1CC),
+              ],
+            ),
+          ),
+          child: Center(
+            child: _buildSessionDataWidget(sessionData!),
+          )),
+    );
+  }
+
+  Widget _buildSessionDataWidget(SessionData sessionData) {
+    return Text(
+      'Access Token: ${sessionData.accessToken}',
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 18,
       ),
     );
   }
